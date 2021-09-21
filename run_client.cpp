57114544a2be01708e1756fb75fa278650bb47a1
chrisbaldwin2 <cgb2@illinois.edu>
2021-09-21 17:25:06 -0500

    Added comments + history table

--- run_client.cpp
+++ run_client.cpp
@@ -1,3 +1,26 @@
+/* run_client.cpp
+ *
+ * Original Author: Chris Baldwin
+ * Partner:         Kaushal Prudhvi
+ * Date:            9/20/2021
+ * Course:          ECEN 602 Fall 2021
+ * Assignmnet:      MP1
+ * Professor:       Dr. Narasimha Annapareddy
+ * TA:              Rishabh Singla
+ * 
+ * History Table
+ * ===========================================================
+ * Date       :: Author        :: Change
+ * -----------++---------------++-----------------------------
+ * 9/20/2021  :: Chris Baldwin :: Created run_client.cpp file
+ * -----------++---------------++-----------------------------
+ * 9/21/2021  :: Chris Baldwin :: Added errno handling
+ * -----------++---------------++-----------------------------
+ * 9/21/2021  :: Chris Baldwin :: Added function header 
+ *            ::               :: comments and history table
+ * -----------++---------------++-----------------------------
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/socket.h>
@@ -8,86 +31,138 @@
 #include<errno.h>
 #include<unistd.h>
 #include<arpa/inet.h>
-// #include<netdb.h>
 #include"mp1.h"
 
+/* error
+ *
+ * Prints the reason for the error and exits the program with code -1
+ * 
+ * @param err The string describing why the error has occured
+ * @return none
+ */
 void error(const char *err)
 {
     printf("%s", err);
     exit(-1);
 }
 
+/* bind_socket
+ *
+ * Acquires a socket
+ * 
+ * @param none
+ * @return sfd The socket file descriptor
+ */
 int get_socket()
 {
-    int sfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sfd < 0) error("Error getting socket");
-    printf("socket descriptor: %d\n", sfd);
+    int sfd;
+    // Attempt to acquire a TCP IPv4 socket
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sfd < 0) error("Error acquiring socket\n");
+    // printf("socket descriptor: %d\n", sfd);
     return sfd;
 }
 
-int connect_to_server(int sfd, char *argv[])
+/* connect_to_server
+ *
+ * Connects to a server specified in argv where argv[1] is the ip 
+ * and argv[2] is the port number
+ * 
+ * @param sfd The open socket file descriptor
+ * @param argv The arguments passed in from the command line
+ * @return none
+ */
+void connect_to_server(int sfd, char *argv[])
 {
-    // struct hostent *server = gethostbyname(argv[1]);
     struct sockaddr_in serv_addr;
     char *str[20];
+    int port, err;
+    // Zero out the memory @ serv_addr
     bzero((char *) &serv_addr, sizeof(serv_addr));
+    // Set values for sockaddr_in struct
     serv_addr.sin_family = AF_INET;
-    // bcopy((char *)server->h_addr, 
-        //  (char *)&serv_addr.sin_addr.s_addr,
-        //  server->h_length);
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    int port = strtol(argv[2], str, 10);
+    port = strtol(argv[2], str, 10);
     printf("port %d\n", port);
     serv_addr.sin_port = htons((uint16_t) port);
-    int err = connect(sfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr));
-    if(err < 0) error("ERROR connecting");
-    return 0;
+    // Try to connect to the ip and port number specified
+    err = connect(sfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    if(err < 0) error("Error connecting to server\n");
 }
 
-int readline(char *buf)
+/* readline
+ *
+ * Reads from stdin and writes it to buf
+ * 
+ * @param[out] buf The buffer to write to 
+ * @return none
+ */
+void readline(char *buf)
 {
     printf("Enter the String: ");
+    // Read from stdin at most the size of the buffer
     fgets(buf, MP1::buf_size, stdin);
-    if(buf[0] == 0) error("Invalid input");
-    return 0;
+    if(buf[0] == 0) error("Invalid input\n");
 }
 
-int writen(int sfd, char *buf)
+/* writen
+ *
+ * Writes the buffer passed in to the connected server
+ * 
+ * @param sfd The open socket file descriptor
+ * @param buf The buffer to be written to the server
+ * @return none
+ */
+void writen(int sfd, char *buf)
 {
+    ssize_t size;
     printf("Sending message to server: %s", buf);
-    ssize_t size;
+    // Attempt to send the packet to the server & retry on EINTR
     write_l:
-      size = send(sfd, buf, strlen(buf), 0);
+      size = send(sfd, buf, MP1::buf_size, 0);
       if(size < 0 && errno == EINTR) goto write_l;
-    if (size < 0) error("ERROR writing to socket");
-    return 0;
+    if(size < 0) error("Error writing to socket\n");
 }
 
-int listen_for_resp(int sfd, char *buf)
+/* listen_for_resp
+ *
+ * Waits for the echo from the server and prints it to
+ * stdout
+ * 
+ * @param[in]  sfd The open socket file descriptor
+ * @param[out] buf The buffer to store the echo resp in
+ * @return none
+ */
+void listen_for_resp(int sfd, char *buf)
 {
-    bzero(buf,MP1::buf_size);
     ssize_t size;
+    bzero(buf,MP1::buf_size);
+    // Attempt to receive the packet from the server & retry on EINTR
     read_l:
       size = recv(sfd, buf, MP1::buf_size, 0);
       if(size < 0 && errno == EINTR) goto read_l;
-    if (size < 0) error("ERROR reading from socket");
+    if(size < 0) error("Error reading from socket\n");
     printf("Got %d chars: %s", (int) size, buf);
-    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     char buf [MP1::buf_size];
-    int sock_fd = get_socket();
+    int sock_fd;
+    if(argc != 3) error("Call must have 2 arguments: IPv4_ADDRESS PORT_NUM\n");
+    sock_fd = get_socket();
     connect_to_server(sock_fd, argv);
     while(1)
     {
         readline(buf);
         // printf("comp:%d\n", memcmp(buf, "exit\n", sizeof("exit")));
-        if(!memcmp(buf, "EOF\n", sizeof("EOF"))) break;
+
+        // If EOF (Sentinal) is typed, break out of the loop and close the connection
+        if(!memcmp(buf, "EOF\n", sizeof("EOF\n"))) break;
         writen(sock_fd, buf);
         listen_for_resp(sock_fd, buf);
     }
+    // Send a buffer with all zero (EOF) to the server to signal the end of transmission
     bzero(buf,MP1::buf_size);
     writen(sock_fd, buf);
     printf("\n");
--- run_server.cpp
+++ run_server.cpp
@@ -1,3 +1,26 @@
+/* run_server.cpp
+ * 
+ * Original Author: Chris Baldwin
+ * Partner:         Kaushal Prudhvi
+ * Date:            9/20/2021
+ * Course:          ECEN 602 Fall 2021
+ * Assignmnet:      MP1
+ * Professor:       Dr. Narasimha Annapareddy
+ * TA:              Rishabh Singla
+ * 
+ * History Table
+ * ===========================================================
+ * Date       :: Author        :: Change
+ * -----------++---------------++-----------------------------
+ * 9/20/2021  :: Chris Baldwin :: Created run_server.cpp file
+ * -----------++---------------++-----------------------------
+ * 9/21/2021  :: Chris Baldwin :: Added errno handling
+ * -----------++---------------++-----------------------------
+ * 9/21/2021  :: Chris Baldwin :: Added function header 
+ *            ::               :: comments and history table
+ * -----------++---------------++-----------------------------
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/socket.h>
@@ -11,61 +34,89 @@
 #include"mp1.h"
 #include<signal.h>
 
+/* error
+ *
+ * Prints the reason for the error and exits the program with code -1
+ * 
+ * @param err The string describing why the error has occured
+ * @return none
+ */
 void error(const char *err)
 {
     printf("%s", err);
     exit(-1);
 }
 
+/* bind_socket
+ *
+ * Acquires a socket, binds it, and begins listening
+ * 
+ * @param address The struct containing the addresses to accept (INADDR_ANY), 
+ *                port to bind, and the family (IPv4)
+ * @return sfd The socket file descriptor
+ */
 int bind_socket(struct sockaddr_in *address)
 {
-    int sfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sfd < 0) error("Error getting socket");
+    int sfd, err;
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sfd < 0) error("Error acquiring socket\n");
     // printf("socket descriptor: %d\n", sfd);
-    int err = bind(sfd ,(struct sockaddr *)address,sizeof(struct sockaddr));
-    if (sfd < 0) error("Error getting socket");
+    err = bind(sfd, (struct sockaddr *) address, sizeof(struct sockaddr));
+    if(sfd < 0) error("Error binding socket\n");
     // printf("bind: %d\n", err);
     err = listen(sfd, MP1::list_queue_size);
-    if(err < 0) error("Error on listen");
-    // Handle socket with interupts
-    // while(err == EINTR) err = listen(sfd, MP1::list_queue_size);
+    if(err < 0) error("Error listening on socket\n");
     // printf("listen: %d\n", err);
     return sfd;
 }
 
-int listen_on_socket(struct sockaddr_in *cli_addr, int sfd, int port)
+/* listen_on_socket
+ *
+ * Listens for a new connenction, accepts it, forks, and echos any packets which are sent.
+ * On sending a blank buffer (EOF), the connection is closed and the child process ended.
+ * 
+ * @param sfd The open socket file descriptor
+ * @param port The port number passed in from command line
+ * @return none
+ */
+void listen_on_socket(int sfd, int port)
 {
     printf("Listening on sock %d port %d\n", sfd, port);
+    sockaddr_in *cli_addr;
+    bzero((char *) cli_addr, sizeof(sockaddr));
     socklen_t clilen = sizeof(sockaddr);
-    char buf[MP1::buf_size];
+    int newsockfd, pid;
     signal(SIGCHLD, SIG_IGN);
     while(1)
     {
-        int newsockfd = accept(sfd, (struct sockaddr *) cli_addr, &clilen);
-        if (newsockfd < 0) error("Accept");
+        newsockfd = accept(sfd, (struct sockaddr *) cli_addr, &clilen);
+        if(newsockfd < 0) error("Error accepting connection\n");
         printf("Opening client %d\n", cli_addr->sin_port);
-        int pid = fork();
-        if(pid < 0) error("Error listening on socket");
+        pid = fork();
+        if(pid < 0) error("Error forking proccess\n");
         if(pid == 0){
             // Child Process
+            char buf[MP1::buf_size];
             char ip_str[INET_ADDRSTRLEN];
+            ssize_t size;
             inet_ntop(AF_INET, &(cli_addr->sin_addr), ip_str, INET_ADDRSTRLEN);
             while(1)
             {
+                // Zero the buffer to prevent reading stale values
                 bzero(buf, MP1::buf_size);
-                ssize_t size;
                 read_l:
                   size = recv(newsockfd, buf, MP1::buf_size, 0);
                   if(size < 0 && errno == EINTR) goto read_l;
-                if(size < 0) error("Receive");
+                if(size < 0) error("Error receiving packet\n");
                 if(size == 0) break;
                 printf("Received @ %s::%d size %d: %s", ip_str, cli_addr->sin_port, (int) size, buf);
                 printf("Echoing: %s", buf);
                 write_l:
                   size = send(newsockfd, buf, MP1::buf_size, 0);
                   if(size < 0 && errno == EINTR) goto write_l;
-                if(size < 0) error("Send"); 
+                if(size < 0) error("Error sending echo packet\n"); 
             }
+            // After receiving EOF, Close the client and end the child process
             printf("Closing client %d\n", cli_addr->sin_port);
             close(newsockfd);
             exit(0);
@@ -73,19 +124,24 @@ int listen_on_socket(struct sockaddr_in *cli_addr, int sfd, int port)
         // Parent Process
         close(newsockfd);
     }
-    return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    sockaddr_in serv_addr, cli_addr;
+    int port, sfd;
+    char *port_str[20];
+    // Handle issues with port number
+    if(argc != 2) error("Call must have 1 argument: PORT_NUM\n");
+    port = strtol(argv[1], port_str, 10);
+    if(port <= 1024) error("Error port cannot be less than 1024\n");
+    // Assign socket struct variables
+    sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    char *str[20];
-    int port = strtol(argv[1], str, 10);
-    if(port < 1024) port = MP1::port_num;
     serv_addr.sin_port = htons((uint16_t) port);
-    int sfd = bind_socket(&serv_addr);
-    listen_on_socket(&cli_addr, sfd, port);
+    // Bind the socket
+    sfd = bind_socket(&serv_addr);
+    // Listen to socket ( infinite loop )
+    listen_on_socket(sfd, port);
     return 0;
 }
